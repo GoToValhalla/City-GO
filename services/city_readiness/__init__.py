cf82da103acb6af8ad3e34202d93df6c1c450f0d
GoToValhalla <56562169+GoToValhalla@users.noreply.github.com>
2026-06-18 19:08:16 +0500

    data-foundation: export city readiness recalculation

--- services/city_readiness/__init__.py
+++ services/city_readiness/__init__.py
@@ -1,4 +1,15 @@
-from services.city_readiness.score import compute_city_readiness
+from services.city_readiness.score import (
+    compute_city_readiness,
+    latest_city_readiness_snapshot,
+    recalculate_all_city_readiness_snapshots,
+    recalculate_city_readiness_snapshot,
+)
 from services.city_readiness.summary import list_cities_readiness
 
-__all__ = ["compute_city_readiness", "list_cities_readiness"]
+__all__ = [
+    "compute_city_readiness",
+    "latest_city_readiness_snapshot",
+    "list_cities_readiness",
+    "recalculate_all_city_readiness_snapshots",
+    "recalculate_city_readiness_snapshot",
+]
