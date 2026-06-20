940870054cd3028491a54984d2a0a97cc3b9a39b
GoToValhalla <56562169+GoToValhalla@users.noreply.github.com>
2026-06-20 16:49:18 +0500

    Show backend version endpoint hint

--- frontend/src/shared/AppVersionBadge.tsx
+++ frontend/src/shared/AppVersionBadge.tsx
@@ -1,5 +1,5 @@
 import { frontendBuildInfo } from './config/buildInfo'
 
 export function AppVersionBadge() {
-  return <div className="app-version-badge">FE {frontendBuildInfo.buildShaShort}</div>
+  return <div className="app-version-badge">FE {frontendBuildInfo.buildShaShort} BE /api/version</div>
 }
