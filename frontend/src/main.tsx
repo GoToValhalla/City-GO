57d65b96794c06bea105058671713527c12038c9
GoToValhalla <56562169+GoToValhalla@users.noreply.github.com>
2026-06-20 16:40:41 +0500

    Mount app version badge

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AppVersionBadge } from './shared/AppVersionBadge'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div>
+      <App />
+      <AppVersionBadge />
+    </div>
   </StrictMode>,
 )
