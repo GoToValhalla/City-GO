317dfef86398bcd28a93649bec03dcb4b4cc46c1
GoToValhalla <56562169+GoToValhalla@users.noreply.github.com>
2026-06-23 14:20:45 +0300

    Load City GO design system styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { AppVersionBadge } from './shared/AppVersionBadge'
+import './styles/design-system.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
