01cd002d77cc4a53ea701e7b76ee59298df19594
GoToValhalla <56562169+GoToValhalla@users.noreply.github.com>
2026-06-23 14:46:50 +0300

    Load design tokens before app styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles/design-system.css'
 import App from './App.tsx'
 import { AppVersionBadge } from './shared/AppVersionBadge'
-import './styles/design-system.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
