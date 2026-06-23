7589e9962d8465925d34dcd69c9e21c4a6ee63d8
GoToValhalla <56562169+GoToValhalla@users.noreply.github.com>
2026-06-23 15:20:33 +0300

    Add category badge

---
+++ frontend/src/components/ui/CategoryBadge.tsx
@@ -0,0 +1,14 @@
+import { categoryLabel } from '../../shared/place/categoryLabels'
+
+type CategoryBadgeProps = {
+  category: string | null | undefined
+  className?: string
+}
+
+const classNames = (...values: Array<string | false | null | undefined>) => values.filter(Boolean).join(' ')
+
+export const CategoryBadge = ({ category, className }: CategoryBadgeProps) => (
+  <span className={classNames('cg-category-badge', className)}>
+    {categoryLabel(category ?? '')}
+  </span>
+)
