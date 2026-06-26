40e24ca749f1a8b26e89dde73bd1d1443454e607
GoToValhalla <56562169+GoToValhalla@users.noreply.github.com>
2026-06-26 18:53:44 +0300

    Export admin preview route eligibility from active package

--- services/route_eligibility/__init__.py
+++ services/route_eligibility/__init__.py
@@ -1,6 +1,7 @@
 from services.route_eligibility.evaluate import RouteEligibilityResult, evaluate_place_route_eligibility
 from services.route_eligibility.forbidden_categories import ALGORITHM_VERSION, ROUTE_FORBIDDEN_CATEGORIES
 from services.route_eligibility.query_filters import (
+    admin_preview_route_eligible_sql_conditions,
     apply_route_eligible_filters,
     is_route_forbidden_category,
     route_eligible_sql_conditions,
@@ -10,6 +11,7 @@ __all__ = [
     "ALGORITHM_VERSION",
     "ROUTE_FORBIDDEN_CATEGORIES",
     "RouteEligibilityResult",
+    "admin_preview_route_eligible_sql_conditions",
     "apply_route_eligible_filters",
     "evaluate_place_route_eligibility",
     "is_route_forbidden_category",
