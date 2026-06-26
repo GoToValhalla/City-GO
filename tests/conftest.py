2582ad8746d0180711f4abf44f0aa8542d5278c6
GoToValhalla <56562169+GoToValhalla@users.noreply.github.com>
2026-06-26 21:39:30 +0300

    Register publication policy models in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -41,9 +41,12 @@ import models.import_batch  # noqa: F401
 import models.import_job_step  # noqa: F401
 import models.place_scope_link  # noqa: F401
 import models.place  # noqa: F401
+import models.place_change_review  # noqa: F401
 import models.place_field_confidence  # noqa: F401
 import models.place_image  # noqa: F401
 import models.place_photo_candidate  # noqa: F401
+import models.place_publication_decision  # noqa: F401
+import models.place_snapshot  # noqa: F401
 import models.place_source_presence  # noqa: F401
 import models.place_tag  # noqa: F401
 import models.route  # noqa: F401
