80392f026c0a28ae85f6db1eb963e58da2913444
GoToValhalla <56562169+GoToValhalla@users.noreply.github.com>
2026-06-26 15:31:19 +0300

    Fix public visibility city publication predicate

--- services/place_public_visibility.py
+++ services/place_public_visibility.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import Any
 
-from sqlalchemy import or_
+from sqlalchemy import and_, or_
 from sqlalchemy.orm import Query
 
 from models.city import City
