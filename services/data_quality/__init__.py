4bb9467e09806c30d928abb16c581ff4bdb6eda7
GoToValhalla <56562169+GoToValhalla@users.noreply.github.com>
2026-06-28 07:24:53 +0300

    Export duplicate quality groups

--- services/data_quality/__init__.py
+++ services/data_quality/__init__.py
@@ -1,5 +1,9 @@
 from services.data_quality.bulk import apply_bulk_action, preview_bulk_action
-from services.data_quality.query import build_data_quality_summary, list_data_quality_issues
+from services.data_quality.query import (
+    build_data_quality_summary,
+    list_data_quality_issues,
+    list_possible_duplicate_groups,
+)
 from services.data_quality.readiness import diagnostic_gates
 from services.data_quality.refresh import refresh_data_quality_issues
 
@@ -8,6 +12,7 @@ __all__ = [
     "build_data_quality_summary",
     "diagnostic_gates",
     "list_data_quality_issues",
+    "list_possible_duplicate_groups",
     "preview_bulk_action",
     "refresh_data_quality_issues",
 ]
