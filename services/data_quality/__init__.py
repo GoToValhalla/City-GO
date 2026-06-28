bc0398f9cea773c6147e8e7df46bf42ab5c9e775
GoToValhalla <56562169+GoToValhalla@users.noreply.github.com>
2026-06-28 08:32:00 +0300

    Export data quality automation service

--- services/data_quality/__init__.py
+++ services/data_quality/__init__.py
@@ -1,3 +1,4 @@
+from services.data_quality.automation import apply_automation, preview_automation, rollback_automation
 from services.data_quality.bulk import apply_bulk_action, preview_bulk_action
 from services.data_quality.query import (
     build_data_quality_summary,
@@ -8,11 +9,14 @@ from services.data_quality.readiness import diagnostic_gates
 from services.data_quality.refresh import refresh_data_quality_issues
 
 __all__ = [
+    "apply_automation",
     "apply_bulk_action",
     "build_data_quality_summary",
     "diagnostic_gates",
     "list_data_quality_issues",
     "list_possible_duplicate_groups",
+    "preview_automation",
     "preview_bulk_action",
     "refresh_data_quality_issues",
+    "rollback_automation",
 ]
