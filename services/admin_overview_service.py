5e1738bd56e9c200890d7d979c0ebc4d464bb046
GoToValhalla <56562169+GoToValhalla@users.noreply.github.com>
2026-07-04 17:43:12 +0300

    Read admin overview through read model

--- services/admin_overview_service.py
+++ services/admin_overview_service.py
@@ -1,5 +1,5 @@
 """Admin overview actionable cards."""
 
-from services.admin_overview_compact import build_admin_overview
+from services.admin_read_model_service import get_admin_overview_read_model as build_admin_overview
 
 __all__ = ["build_admin_overview"]
