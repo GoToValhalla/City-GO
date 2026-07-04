57322b7d149c137e06f5a9b1ed15083baaeaf025
GoToValhalla <56562169+GoToValhalla@users.noreply.github.com>
2026-07-04 20:00:35 +0300

    Read overview through snapshot fallback

--- services/admin_overview_service.py
+++ services/admin_overview_service.py
@@ -1,5 +1,5 @@
 """Admin overview actionable cards."""
 
-from services.admin_overview_compact import build_admin_overview
+from services.admin_read_model_v2 import admin_overview as build_admin_overview
 
 __all__ = ["build_admin_overview"]
