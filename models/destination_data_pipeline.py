3ee53820489ee46f782004cb6356a56e6c672e66
GoToValhalla <56562169+GoToValhalla@users.noreply.github.com>
2026-07-07 09:12:42 +0300

    Register destination model for pipeline relationship

--- models/destination_data_pipeline.py
+++ models/destination_data_pipeline.py
@@ -10,6 +10,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.types import JSON
 
 from db.base import Base
+from models.destination import Destination  # noqa: F401
 
 
 def utc_now() -> datetime:
